396dba41bac760bef5094ea465d3f8e692127a73
Shawn Smith <shawnps@fastly.com>
2022-05-23 17:30:00 +0900

    cmd/fastly-exporter: move roundTripperFunc, userAgentTransport into main.go, allowing "go run cmd/fastly-exporter/main.go" (#105)

--- cmd/fastly-exporter/main.go
+++ cmd/fastly-exporter/main.go
@@ -473,3 +473,14 @@ func getLogLevel(debug bool) level.Option {
 		return level.AllowInfo()
 	}
 }
+
+type roundTripperFunc func(*http.Request) (*http.Response, error)
+
+func (f roundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) { return f(req) }
+
+func userAgentTransport(next http.RoundTripper, userAgent string) http.RoundTripper {
+	return roundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		req.Header.Set("User-Agent", userAgent)
+		return next.RoundTrip(req)
+	})
+}
